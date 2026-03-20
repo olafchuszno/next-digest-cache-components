f7bb5b2e71c98524939dd569bc549f53561a974f
Olaf <olaf.chuszno@gmail.com>
2026-03-21 00:09:19 +0400

    style: Add border

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 
 const PAGES = [
-  { href: '/cache', label: 'Cache' },
-  { href: '/cache-tag', label: 'Cache Tag' },
-  { href: '/create-post', label: 'Create Post' },
-  { href: '/revalidate', label: 'Revalidate' },
   { href: '/static-route', label: 'Static Route' },
+  { href: '/cache', label: 'Cache' },
   { href: '/suspense', label: 'Suspense' },
+  { href: '/create-post', label: 'Create Post' },
+  { href: '/cache-tag', label: 'Cache Tag' },
+  { href: '/revalidate', label: 'Revalidate' },
 ];
 
 export default function Page() {
@@ -18,7 +18,7 @@ export default function Page() {
 
       <nav className="flex flex-col mt-4 gap-2">
         {PAGES.map(({ href, label }) => (
-          <Link key={href} href={href} className="w-fit underline">
+          <Link key={href} href={href} className="w-fit bordered">
             {label}
           </Link>
         ))}
